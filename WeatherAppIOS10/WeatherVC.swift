6b88d2586037576c9fd01e246adf0327ab3d3465
marvin evins <maevins@cps.edu>
2016-08-17 22:10:59 -0500

    added table view configuration

--- WeatherAppIOS10/WeatherVC.swift
+++ WeatherAppIOS10/WeatherVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var dateLabel: UILabel!
     
@@ -26,12 +26,26 @@ class WeatherVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+  
+        
     }
 
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+           return cell
+    }
 
 }
 
