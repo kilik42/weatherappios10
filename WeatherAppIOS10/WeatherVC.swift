2aca349a1235bd4acb677bfdb3a2430468459a2e
marvin evins <maevins@cps.edu>
2016-08-17 21:39:36 -0500

    initial outlets configs with stack views

---
+++ WeatherAppIOS10/WeatherVC.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  WeatherAppIOS10
+//
+//  Created by marvin evins on 8/15/16.
+//  Copyright © 2016 marvin evins. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet var dateLabel: UILabel!
+    
+    @IBOutlet var currentTempLabel: UILabel!
+    
+    @IBOutlet var locationLabel: UILabel!
+    
+    @IBOutlet var currentWeatherIcon: UIImageView!
+    
+    
+    @IBOutlet var tableView: UITableView!
+    
+    @IBOutlet var currentWeatherLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    
+    
+
+}
+
